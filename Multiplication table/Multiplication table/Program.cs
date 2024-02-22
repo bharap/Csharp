a0188ab61bcea58e7ff4923023b7e9a24a4f883b
Siva Prakash <sivaprakashnetti@gmail.com>
2024-02-22 18:44:22 +0530

    program_1

---
+++ Multiplication table/Multiplication table/Program.cs
@@ -0,0 +1,8 @@
+﻿int n, i;
+Console.WriteLine("Enter a number");
+n = Convert.ToInt32(Console.ReadLine());
+for(i = 0; i <= 20;i++)
+{
+    Console.Write("{0} X {1} = {2}\n", n, i, n * i);
+}
+
---
+++ sumOfOddNum/sumOfOddNum/Program.cs
@@ -0,0 +1,11 @@
+﻿int n, i, sum = 0;
+n = Convert.ToInt32(Console.ReadLine());
+for (i = 1;i<n;i++)
+{
+    if (i % 2 == 1)
+    {
+        Console.WriteLine(i);
+        sum = sum + i;
+    }
+}
+Console.WriteLine(sum);
