a69a09940e02bb34caa960f5a900a700421f28eb
Siva Prakash <sivaprakashnetti@gmail.com>
2024-02-22 23:24:41 +0530

    Commit Palindrome

---
+++ Palindrome/Palindrome/Program.cs
@@ -0,0 +1,20 @@
+﻿int n,rem,s=0,t;
+Console.WriteLine("Enter a number");
+n= Convert.ToInt32(Console.ReadLine());
+t = n;
+while (n > 0)
+{
+    rem = (n % 10);
+    s = (s * 10) + rem;
+    n = n / 10;
+}
+
+if (s == t)
+{
+    Console.Write("Palindrome");
+}
+else
+{
+    Console.WriteLine("Not a palindrome");
+}
+   
