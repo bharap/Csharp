e98f2da3423067de42990f2d525e5ba123a92d77
Siva Prakash <sivaprakashnetti@gmail.com>
2024-03-12 18:14:04 +0530

    File commit

---
+++ FileRead/FileRead/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+namespace FileRead
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*string path = "C:\\files\\file1.txt";
+            string txt = "Hello";
+            File.WriteAllText(path, txt);*/
+            string path = "C:\\files\\file1.txt";
+            string txt = File.ReadAllText(path);
+            Console.WriteLine(txt);
+        }
+    }
+}
